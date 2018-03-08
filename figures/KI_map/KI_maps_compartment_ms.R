787de245118d06088a134ecf4d1e38ebf14c4f04
daniclaar <danielle.claar@gmail.com>
2018-03-08 12:37:11 -0800

    clean up script

--- figures/KI_map/KI_maps_compartment_ms.R
+++ figures/KI_map/KI_maps_compartment_ms.R
@@ -45,28 +45,3 @@ segments(-157.563, 1.72,-157.563, 1.82)
 # text(-157.53, 1.82, "Vaskess\nBay", cex = 0.5)
 
 dev.off()
-
-
-# 
-# ############### same thing as before but making the text bigger ####################
-# source("figures/KI_map/KI_base_B&W.R")
-# 
-# # village markers sized by population
-# symbols(villages$lon, villages$lat, circles=(villages$pop)/10, add=TRUE,inches=0.3, bg=alpha("red", 0.4))
-# ## legend for village size
-# text(-157.19, 1.85, "1500 people", cex=0.66)   
-# text(-157.19, 1.877, "1000 people", cex=0.66)   
-# text(-157.1945, 1.904, "500 people", cex=0.66)  
-# points(sites$lon, sites$lat, bg=alpha(sites$col,0.8), pch=21, cex=1.4) 
-# #with(sites_compartment, text(lon, lat, label=site, cex=0.7))
-# legend(-157.265, 2.075,legend=levels(sites$f.pressure), pt.bg=c("#8c510a","#d8b365","#c7eae5","#5ab4ac","#01665e"), pch=21, bty="n", pt.cex=1.4, cex=0.6)
-# text(-157.275, 2.01, "Human disturbance", srt=90, cex=0.6)
-# segments(-157.263, 1.968,-157.263, 2.057)
-# text(-157.315, 1.88, "Bay of\nWrecks", cex = 0.45)
-# text(-157.524, 1.825, "Vaskess\nBay", cex = 0.45)
-# par(mar=c(1.3,0.9,0.5,0.5))
-# source("ki_map_files/KI_base_inset_forbigger.R")
-# 
-# dev.off()
-# 
-# 
