03426c5b3583708e85baf3a66f3cda36ddaf22cc
daniclaar <danielle.claar@gmail.com>
2018-06-24 11:36:11 -0700

    finalize fig 3

--- figures/Figure_3_pcoa_by_dist.R
+++ figures/Figure_3_pcoa_by_dist.R
@@ -194,3 +194,105 @@ mtext("f)", side=3,line=0.5,adj=-0.025,cex=2)
 
 
 dev.off()
+
+
+
+pdf(file="figures/Figure_3_pcoa_by_dist.pdf", 
+     width = 12, height = 8)
+par(mfrow=c(2,3), mar=c(1,1,3,1))
+# Pocillopora
+plot(Peyd.bd.dist, hull=F, label=F, 
+     main=expression(paste(italic("Pocillopora grandis"))), col=sitecols,
+     xlab="", ylab="", cex=2, sub="", 
+     xaxt='n',yaxt='n',cex.main=2.5)
+ordihull(Peyd.bd.dist, sample_data(phy97.f.c.coral.Peyd)$Dist,  
+         draw = c("polygon"), col = sitecols, alpha=0.2, lwd=0.05)
+legend("topright", bty="n", pch=1:6, cex=2.5,
+       legend=levels(as.factor(sample_data(phy97.f.c.coral.Peyd)$Dist)),
+       col=sitecols)
+text(-0.07,-0.037,"betadisper = sig. ***",cex=2)
+text(-0.07,-0.046,"adonis = sig. ***",cex=2)
+mtext("a)", side=3,line=0.5,adj=-0.025,cex=2) 
+
+
+# Montipora
+plot(MAeq.bd.dist, hull=F, label=F, 
+     main=expression(paste(italic("  Montipora aequituburculata"))), col=sitecols,
+     xlab="", ylab="", cex=2, sub="", 
+     xaxt='n',yaxt='n',cex.main=2.5)
+ordihull(MAeq.bd.dist, sample_data(phy97.f.c.coral.MAeq)$Dist, 
+         draw = c("polygon"),
+         col = sitecols, alpha=0.2, lwd=0.05)
+# legend("topleft", bty="n", pch=1:6, cex=2.5,
+#        legend=levels(as.factor(sample_data(phy97.f.c.coral.MAeq)$Dist)),
+#        col=sitecols)
+text(-0.0006,-0.00188,"betadisper = sig. ***",cex=2)
+text(-0.0006,-0.00235,"adonis = sig. *",cex=2)
+mtext("b)", side=3,line=0.5,adj=-0.025,cex=2) 
+
+
+# Porites
+plot(Plob.bd.dist, hull=F, label=F, 
+     main=expression(paste(italic("Porites lobata"))), col=sitecols,
+     xlab="", ylab="", cex=2, sub="", 
+     xaxt='n',yaxt='n',cex.main=2.5)
+ordihull(Plob.bd.dist, sample_data(phy97.f.c.coral.Plob)$Dist, 
+         draw = c("polygon"),
+         col = sitecols, alpha=0.2, lwd=0.05)
+# legend("topleft", bty="n", pch=1:6, cex=2.5,
+#        legend=levels(as.factor(sample_data(phy97.f.c.coral.Plob)$Dist)),
+#        col=sitecols)
+text(-0.00275,-0.018,"betadisper = not sig.",cex=2)
+text(-0.00275,-0.02,"adonis = not sig.",cex=2)
+mtext("c)", side=3,line=0.5,adj=-0.025,cex=2) 
+
+
+# Sediment
+plot(sediment.bd.dist, hull=F, label=F, 
+     main="Sediment", col=sitecols,
+     xlab="", ylab="", cex=2, sub="", 
+     xaxt='n',yaxt='n',cex.main=2.5)
+ordihull(sediment.bd.dist, sample_data(phy97.f.c.sediment)$Dist, 
+         draw = c("polygon"),
+         col = sitecols, alpha=0.2, lwd=0.05)
+# legend("topleft", bty="n", pch=1:6, cex=2.5,
+#        legend=levels(as.factor(sample_data(phy97.f.c.sediment)$Dist)),
+#        col=sitecols)
+text(-0.02,-0.075,"betadisper = sig. ***",cex=2)
+text(-0.02,-0.087,"adonis = sig. ***",cex=2)
+mtext("d)", side=3,line=0.5,adj=-0.025,cex=2) 
+
+
+# Water
+plot(water.bd.dist, hull=F, label=F, 
+     main="Water", col=sitecols,
+     xlab="", ylab="", cex=2, sub="", 
+     xaxt='n',yaxt='n',cex.main=2.5)
+ordihull(water.bd.dist, sample_data(phy97.f.c.water)$Dist, 
+         draw = c("polygon"),
+         col = sitecols, alpha=0.2, lwd=0.05)
+# legend("topleft", bty="n", pch=1:6, cex=2.5,
+#        legend=levels(as.factor(sample_data(phy97.f.c.water)$Dist)),
+#        col=sitecols)
+text(0.045,-0.040,"betadisper = not sig.",cex=2)
+text(0.045,-0.047,"adonis = sig. **",cex=2)
+mtext("e)", side=3,line=0.5,adj=-0.025,cex=2) 
+
+
+# All
+plot(all.bd.dist, hull=F, label=F, 
+     main="All Compartments", col=sitecols,
+     xlab="", ylab="", cex=2, sub="", 
+     xaxt='n',yaxt='n',cex.main=2.5)
+ordihull(all.bd.dist, sample_data(phy97.f.c)$Dist, 
+         draw = c("polygon"),
+         col = sitecols, alpha=0.2, lwd=0.05)
+# legend("topleft", bty="n", pch=1:6, cex=2.5,
+#        legend=levels(as.factor(sample_data(phy97.f.c.water)$Dist)),
+#        col=sitecols)
+text(0.065,-0.027,"betadisper = sig. ***",cex=2)
+text(0.065,-0.037,"adonis = sig. ***",cex=2) 
+mtext("f)", side=3,line=0.5,adj=-0.025,cex=2) 
+
+
+dev.off()
