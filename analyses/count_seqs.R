1efa2008a8052140692fd1dfbfcbad6b42812275
daniclaar <danielle.claar@gmail.com>
2019-06-10 22:46:59 -0700

    cleanup and re-run count seqs

--- analyses/count_seqs.R
+++ analyses/count_seqs.R
@@ -8,13 +8,13 @@ load("data/KI_Compartment_f_coral_grouped.RData")
 load("analyses/KI_Compartment_colors.RData")
 
 # Calculate the total number of sequences
-total_seqs <- sum(taxa_sums(phy97.f.c))
+total_seqs <- sum(taxa_sums(phyASV.f.c))
 total_seqs
 
 # Calculate the number of sequences in each compartment
-coral_seqs <- sum(taxa_sums(phy97.f.c.coral))
-sediment_seqs <- sum(taxa_sums(phy97.f.c.sediment))
-water_seqs <- sum(taxa_sums(phy97.f.c.water))
+coral_seqs <- sum(taxa_sums(phyASV.f.c.coral))
+sediment_seqs <- sum(taxa_sums(phyASV.f.c.sediment))
+water_seqs <- sum(taxa_sums(phyASV.f.c.water))
 
 # Check that total_seqs equals the sum of all compartments combined
 coral_seqs+sediment_seqs+water_seqs
