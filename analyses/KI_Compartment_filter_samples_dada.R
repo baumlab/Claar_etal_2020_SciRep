28ce20acd53cd3726b6ba53022a8a9079852f93a
daniclaar <danielle.claar@gmail.com>
2019-06-10 22:06:50 -0700

    re-run filtering pipeline after ASV pipeline is complete

--- analyses/KI_Compartment_filter_samples_dada.R
+++ analyses/KI_Compartment_filter_samples_dada.R
@@ -14,6 +14,8 @@ rm(list=ls())
 # Load filtered RData object from output of filter_notsym.R script
 load("analyses/KI_Compartment_dada.RData")
 
+phy.f <- ps
+
 sample_data(phy.f)$field_season <- sample_data(phy.f)$Year_Pre_Post
 sample_data(phy.f)$field_season <- as.character(sample_data(phy.f)$field_season)
 sample_data(phy.f)$field_season <- replace(sample_data(phy.f)$field_season, sample_data(phy.f)$field_season=="2014", "KI2014")
@@ -89,7 +91,7 @@ ASV.As <- subset_taxa(phyASV.f.c,Genus=="g__Symbiodinium")
 ASV.Bs <- subset_taxa(phyASV.f.c,Genus=="g__Breviolum")
 ASV.Cs <- subset_taxa(phyASV.f.c,Genus=="g__Cladocopium")
 ASV.Ds <- subset_taxa(phyASV.f.c,Genus=="g__Durusdinium")
-ASV.Es <- subset_taxa(phyASV.f.c,Genus=="g__Effrenium")
+# ASV.Es <- subset_taxa(phyASV.f.c,Genus=="g__Effrenium")
 ASV.Fs <- subset_taxa(phyASV.f.c,Genus=="g__Fugacium")
 ASV.Gs <- subset_taxa(phyASV.f.c,Genus=="g__Gerakladium")
 # ASV.Hs <- subset_taxa(phyASV.f.c,Genus=="g__cladeH") # no seqs
@@ -100,58 +102,50 @@ min(width(ASV.seqs))
 max(width(ASV.seqs))
 
 ASV.A.seqs <- refseq(ASV.As)
+writeXStringSet(ASV.A.seqs, # write to a new FASTA file
+                file="data/Bioinf/tree/ASV_A_tree_seqs.fasta")
 ASV.A.seqs.aligned <- AlignSeqs(ASV.A.seqs)
 writeXStringSet(ASV.A.seqs.aligned, # write the alignment to a new FASTA file
                 file="data/Bioinf/tree/ASV_A_tree_seqs_aligned.fasta")
 ASV.B.seqs <- refseq(ASV.Bs)
+writeXStringSet(ASV.B.seqs, # write to a new FASTA file
+                file="data/Bioinf/tree/ASV_B_tree_seqs.fasta")
 ASV.B.seqs.aligned <- refseq(ASV.Bs) # There is only one sequence here! Can't align
 writeXStringSet(ASV.B.seqs.aligned, # write the alignment to a new FASTA file
                 file="data/Bioinf/tree/ASV_B_tree_seqs_aligned.fasta")
 ASV.C.seqs <- refseq(ASV.Cs)
+writeXStringSet(ASV.C.seqs, # write to a new FASTA file
+                file="data/Bioinf/tree/ASV_C_tree_seqs.fasta")
 ASV.C.seqs.aligned <- AlignSeqs(ASV.C.seqs)
 writeXStringSet(ASV.C.seqs.aligned, # write the alignment to a new FASTA file
                 file="data/Bioinf/tree/ASV_C_tree_seqs_aligned.fasta")
 ASV.D.seqs <- refseq(ASV.Ds)
+writeXStringSet(ASV.D.seqs, # write to a new FASTA file
+                file="data/Bioinf/tree/ASV_D_tree_seqs.fasta")
 ASV.D.seqs.aligned <- AlignSeqs(ASV.D.seqs)
 writeXStringSet(ASV.D.seqs.aligned, # write the alignment to a new FASTA file
                 file="data/Bioinf/tree/ASV_D_tree_seqs_aligned.fasta")
-ASV.E.seqs <- refseq(ASV.Es)
-writeXStringSet(ASV.E.seqs, # write the alignment to a new FASTA file
-                file="data/Bioinf/tree/ASV_Es.fasta")
-ASV.E.seqs.aligned <- AlignSeqs(ASV.E.seqs)
-writeXStringSet(ASV.E.seqs.aligned, # write the alignment to a new FASTA file
-                file="data/Bioinf/tree/ASV_E_tree_seqs_aligned.fasta")
 ASV.F.seqs <- refseq(ASV.Fs)
+writeXStringSet(ASV.F.seqs, # write to a new FASTA file
+                file="data/Bioinf/tree/ASV_F_tree_seqs.fasta")
 ASV.F.seqs.aligned <- AlignSeqs(ASV.F.seqs)
 writeXStringSet(ASV.F.seqs.aligned, # write the alignment to a new FASTA file
                 file="data/Bioinf/tree/ASV_F_tree_seqs_aligned.fasta")
 ASV.G.seqs <- refseq(ASV.Gs)
+writeXStringSet(ASV.G.seqs, # write to a new FASTA file
+                file="data/Bioinf/tree/ASV_G_tree_seqs.fasta")
 ASV.G.seqs.aligned <- AlignSeqs(ASV.G.seqs)
 writeXStringSet(ASV.G.seqs.aligned, # write the alignment to a new FASTA file
                 file="data/Bioinf/tree/ASV_G_tree_seqs_aligned.fasta")
 ASV.I.seqs <- refseq(ASV.Is)
+writeXStringSet(ASV.I.seqs, # write to a new FASTA file
+                file="data/Bioinf/tree/ASV_I_tree_seqs.fasta")
 ASV.I.seqs.aligned <- AlignSeqs(ASV.I.seqs)
 writeXStringSet(ASV.I.seqs.aligned, # write the alignment to a new FASTA file
                 file="data/Bioinf/tree/ASV_I_tree_seqs_aligned.fasta")
 
-min(width(ASV.A.seqs))
-max(width(ASV.A.seqs))
-min(width(ASV.B.seqs))
-max(width(ASV.B.seqs))
-min(width(ASV.C.seqs))
-max(width(ASV.C.seqs))
-min(width(ASV.D.seqs))
-max(width(ASV.D.seqs))
-min(width(ASV.E.seqs))
-max(width(ASV.E.seqs))
-min(width(ASV.F.seqs))
-max(width(ASV.F.seqs))
-min(width(ASV.G.seqs))
-max(width(ASV.G.seqs))
-min(width(ASV.I.seqs))
-max(width(ASV.I.seqs))
-
-
+writeXStringSet(c(ASV.A.seqs.aligned,ASV.B.seqs.aligned,ASV.C.seqs.aligned,ASV.D.seqs.aligned,ASV.F.seqs.aligned,ASV.G.seqs.aligned,ASV.I.seqs.aligned), 
+                file="data/Bioinf/tree/ASV_ALL_tree_seqs_aligned.fasta")
 
 #https://rdrr.io/rforge/seqinr/man/dist.alignment.html
 #returns sqrt of pairwise genetic distance, then squared the matrices
@@ -171,10 +165,6 @@ D.seqs <- read.alignment(file = "data/Bioinf/tree/ASV_D_tree_seqs_aligned.fasta"
 D.dis <- (as.matrix(dist.alignment(D.seqs, matrix = "identity" )))^2
 write.csv(D.dis, file="data/Bioinf/tree/ASV_D_dis_matx.csv")
 
-E.seqs <- read.alignment(file = "data/BioinE/tree/ASV_E_tree_seqs_aligned.fasta", format= "fasta")
-E.dis <- (as.matrix(dist.alignment(E.seqs, matrix = "identity" )))^2
-write.csv(E.dis, file="data/Bioinf/tree/ASV_E_dis_matx.csv")
-
 F.seqs <- read.alignment(file = "data/Bioinf/tree/ASV_F_tree_seqs_aligned.fasta", format= "fasta")
 F.dis <- (as.matrix(dist.alignment(F.seqs, matrix = "identity" )))^2
 write.csv(F.dis, file="data/Bioinf/tree/ASV_F_dis_matx.csv")
@@ -191,28 +181,28 @@ write.csv(I.dis, file="data/Bioinf/tree/ASV_I_dis_matx.csv")
 A_B <- matrix(0.219, ncol=ncol(A.dis), nrow=nrow(B.dis), dimnames=list(rownames(B.dis), colnames(A.dis)))
 A_C <- matrix(0.1960, ncol=ncol(A.dis), nrow=nrow(C.dis), dimnames=list(rownames(C.dis), colnames(A.dis)))
 A_D <- matrix(0.1775, ncol=ncol(A.dis), nrow=nrow(D.dis), dimnames=list(rownames(D.dis), colnames(A.dis)))
-A_E <- matrix(0.2085, ncol=ncol(A.dis), nrow=nrow(E.dis), dimnames=list(rownames(E.dis), colnames(A.dis)))
+# A_E <- matrix(0.2085, ncol=ncol(A.dis), nrow=nrow(E.dis), dimnames=list(rownames(E.dis), colnames(A.dis)))
 A_F <- matrix(0.2085, ncol=ncol(A.dis), nrow=nrow(F.dis), dimnames=list(rownames(F.dis), colnames(A.dis)))
 A_G <- matrix(0.216, ncol=ncol(A.dis), nrow=nrow(G.dis), dimnames=list(rownames(G.dis), colnames(A.dis)))
 A_I <- matrix(0.205, ncol=ncol(A.dis), nrow=nrow(I.dis), dimnames=list(rownames(I.dis), colnames(A.dis)))
 B_C <- matrix(0.114, ncol=ncol(B.dis), nrow=nrow(C.dis), dimnames=list(rownames(C.dis), colnames(B.dis)))
 B_D <- matrix(0.1705, ncol=ncol(B.dis), nrow=nrow(D.dis), dimnames=list(rownames(D.dis), colnames(B.dis)))
-B_E <- matrix(0.1355, ncol=ncol(B.dis), nrow=nrow(E.dis), dimnames=list(rownames(E.dis), colnames(B.dis)))
+# B_E <- matrix(0.1355, ncol=ncol(B.dis), nrow=nrow(E.dis), dimnames=list(rownames(E.dis), colnames(B.dis)))
 B_F <- matrix(0.1355, ncol=ncol(B.dis), nrow=nrow(F.dis), dimnames=list(rownames(F.dis), colnames(B.dis)))
 B_G <- matrix(0.21, ncol=ncol(B.dis), nrow=nrow(G.dis), dimnames=list(rownames(G.dis), colnames(B.dis)))
 B_I <- matrix(0.151, ncol=ncol(B.dis), nrow=nrow(I.dis), dimnames=list(rownames(I.dis), colnames(B.dis)))
 C_D <- matrix(0.1520, ncol=ncol(C.dis), nrow=nrow(D.dis), dimnames=list(rownames(D.dis), colnames(C.dis)))
-C_E <- matrix(0.0945, ncol=ncol(C.dis), nrow=nrow(E.dis), dimnames=list(rownames(E.dis), colnames(C.dis)))
+# C_E <- matrix(0.0945, ncol=ncol(C.dis), nrow=nrow(E.dis), dimnames=list(rownames(E.dis), colnames(C.dis)))
 C_F <- matrix(0.0945, ncol=ncol(C.dis), nrow=nrow(F.dis), dimnames=list(rownames(F.dis), colnames(C.dis)))
 C_G <- matrix(0.187, ncol=ncol(C.dis), nrow=nrow(G.dis), dimnames=list(rownames(G.dis), colnames(C.dis)))
 C_I <- matrix(0.137, ncol=ncol(C.dis), nrow=nrow(I.dis), dimnames=list(rownames(I.dis), colnames(C.dis)))
-D_E <- matrix(0.1691, ncol=ncol(D.dis), nrow=nrow(E.dis), dimnames=list(rownames(E.dis), colnames(D.dis)))
+# D_E <- matrix(0.1691, ncol=ncol(D.dis), nrow=nrow(E.dis), dimnames=list(rownames(E.dis), colnames(D.dis)))
 D_F <- matrix(0.1691, ncol=ncol(D.dis), nrow=nrow(F.dis), dimnames=list(rownames(F.dis), colnames(D.dis)))
 D_G <- matrix(0.1795, ncol=ncol(D.dis), nrow=nrow(G.dis), dimnames=list(rownames(G.dis), colnames(D.dis)))
 D_I <- matrix(0.169125, ncol=ncol(D.dis), nrow=nrow(I.dis), dimnames=list(rownames(I.dis), colnames(D.dis)))
-E_F <- matrix(0.1691, ncol=ncol(E.dis), nrow=nrow(F.dis), dimnames=list(rownames(F.dis), colnames(E.dis)))
-E_G <- matrix(0.1691, ncol=ncol(E.dis), nrow=nrow(G.dis), dimnames=list(rownames(G.dis), colnames(E.dis)))
-E_I <- matrix(0.1691, ncol=ncol(E.dis), nrow=nrow(I.dis), dimnames=list(rownames(I.dis), colnames(E.dis)))
+# E_F <- matrix(0.1691, ncol=ncol(E.dis), nrow=nrow(F.dis), dimnames=list(rownames(F.dis), colnames(E.dis)))
+# E_G <- matrix(0.1691, ncol=ncol(E.dis), nrow=nrow(G.dis), dimnames=list(rownames(G.dis), colnames(E.dis)))
+# E_I <- matrix(0.1691, ncol=ncol(E.dis), nrow=nrow(I.dis), dimnames=list(rownames(I.dis), colnames(E.dis)))
 F_G <- matrix(0.2072, ncol=ncol(F.dis), nrow=nrow(G.dis), dimnames=list(rownames(G.dis), colnames(F.dis)))
 F_I <- matrix(0.14925, ncol=ncol(F.dis), nrow=nrow(I.dis), dimnames=list(rownames(I.dis), colnames(F.dis)))
 G_I <- matrix(0.194, ncol=ncol(G.dis), nrow=nrow(I.dis), dimnames=list(rownames(I.dis), colnames(G.dis)))
@@ -230,19 +220,17 @@ ubermatrix <- cbind(col1, col2, col3, col4, col5, col6, col7)
 dim(ubermatrix)
 
 #build tree
-uber.tree <- phangorn::upgma(ubermatrix)
+uber.tree <- phangorn::NJ(ubermatrix)
 plot(uber.tree, main="UPGMA")
 
 #write tree to file
 write.tree(uber.tree, file="data/Bioinf/tree/uber.tre")
 
-#use tree and OTU table for calculating beta_diversity.py
-#http://qiime.org/scripts/beta_diversity.html
-
 class(uber.tree)
 
 # Slot uber tree into the phy_tree slot of the phyloseq object
 phy_tree(phyASV.f.c) <- phy_tree(uber.tree)
+plot_tree(phyASV.f.c, label.tips = "Genus", color = "Genus")
 
 ######################### Subset by compartment and calc seqs ##################################
 
