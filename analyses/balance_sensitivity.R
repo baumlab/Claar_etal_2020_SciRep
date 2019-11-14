5f721e5550db207b8efafc00de8d8bf18f04b01d
daniclaar <danielle.claar@gmail.com>
2019-11-14 10:39:16 -0800

    start sensitivity test - balanced design

---
+++ analyses/balance_sensitivity.R
@@ -0,0 +1,56 @@
+# Clear working environment
+rm(list=ls())
+
+# Load necessary packages
+library(phyloseq)
+library(dplyr)
+library(vegan)
+library(purrr)
+
+# Load necessary data
+load("data/KI_Compartment_f_coral_grouped.RData")
+
+physeq<-phyASV.f.c.sediment
+nreps<-34
+nbal<-33
+  
+
+# subset_balanced <- function(physeq,nreps,nbal) {
+  ado.F.list <- list()
+  ado.R2.list <- list()
+  ado.P.list <- list()
+  
+  for(n in 1:nreps){
+  subbal <- sample_data(physeq) %>% 
+  group_by(Dist) %>% 
+  sample_n(size = nbal)
+  # nrow(subbal[subbal$Dist=="VeryHigh",])
+  # nrow(subbal[subbal$Dist=="Medium",])
+
+  phy_sub <- subset_samples(physeq,
+                sample_data(physeq)$SampleID %in% subbal$SampleID)
+  # nrow(sample_data(phy_sub)[sample_data(phy_sub)$Dist=="VeryHigh",])
+  # nrow(sample_data(phy_sub)[sample_data(phy_sub)$Dist=="Medium",])
+
+  set.seed(2020)
+  ufdist <- UniFrac(phy_sub, weighted=T, 
+                             normalized=F, parallel=F, fast=T)
+  # sediment.bd.dist <- betadisper(d=ufdist, 
+  #                                group=sample_data(phy_sub)$Dist,
+  #                                type="centroid", bias.adjust=FALSE)
+  metadata <- as(sample_data(phy_sub), "data.frame")
+  ado <- adonis(ufdist ~ Dist, data = metadata)
+  set.seed(NULL)
+
+  ado.F.list[[n]] <- ado$aov.tab$F.Model[1]
+  ado.R2.list[[n]] <- ado$aov.tab$R2[1]
+  ado.P.list[[n]] <- ado$aov.tab$`Pr(>F)`[1]
+  }
+  ado.F <- do.call(rbind,ado.F.list)
+  ado.R2 <- do.call(rbind,ado.R2.list)
+  ado.P <- do.call(rbind,ado.P.list)
+  ado.stats <- do.call(cbind, list(ado.F, ado.R2, ado.P))
+# }
+# 
+# subset_balanced(phyASV.f.c.sediment,3,33)
+ado.stats
---
+++ analyses/permanova_betadisper_sensitivity.R
@@ -0,0 +1,39 @@
+# KI Compartment Betadispersion - Sensitivity
+
+rm(list=ls())
+load("data/KI_Compartment_f_coral_grouped.RData")
+load("analyses/KI_Compartment_colors.RData")
+
+library(vegan)
+library(ggplot2)
+library(phyloseq)
+
+phyASV.f.c.coral.Peyd <- subset_samples(phyASV.f.c.coral,sample_data(phyASV.f.c.coral)$Coral_Species=="Pocillopora_eydouxi")
+phyASV.f.c.coral.MAeq <- subset_samples(phyASV.f.c.coral,sample_data(phyASV.f.c.coral)$Coral_Species=="Montipora_foliosa")
+phyASV.f.c.coral.Plob <- subset_samples(phyASV.f.c.coral,sample_data(phyASV.f.c.coral)$Coral_Species=="Porites_lobata")
+
+phyASV.f.c.M <- subset_samples(phyASV.f.c,sample_data(phyASV.f.c)$Dist=="Medium")
+phyASV.f.c.VH <- subset_samples(phyASV.f.c,sample_data(phyASV.f.c)$Dist=="VeryHigh")
+
+phyASV.f.c.sediment.2014 <- subset_samples(phyASV.f.c.sediment,sample_data(phyASV.f.c.sediment)$field_season=="KI2014")
+phyASV.f.c.sediment.2015a <- subset_samples(phyASV.f.c.sediment,sample_data(phyASV.f.c.sediment)$field_season=="KI2015a_Post")
+phyASV.f.c.sediment.2015b <- subset_samples(phyASV.f.c.sediment,sample_data(phyASV.f.c.sediment)$field_season=="KI2015b")
+
+
+nrow(sample_data(phyASV.f.c.sediment.M)) #33
+nrow(sample_data(phyASV.f.c.sediment.VH)) #34
+nrow(sample_data(phyASV.f.c.water.M)) #25
+nrow(sample_data(phyASV.f.c.water.VH)) #25
+# nrow(sample_data(phyASV.f.c.coral.MAeq.M)) #40 # dont need adnois is insig.
+# nrow(sample_data(phyASV.f.c.coral.MAeq.VH)) #38
+nrow(sample_data(phyASV.f.c.coral.Peyd.M)) #39
+nrow(sample_data(phyASV.f.c.coral.Peyd.VH)) #31
+# nrow(sample_data(phyASV.f.c.coral.Plob.M)) #34 # dont need, both are insig.
+# nrow(sample_data(phyASV.f.c.coral.Plob.VH)) #39
+nrow(sample_data(phyASV.f.c.M)) #171
+nrow(sample_data(phyASV.f.c.VH)) #167
+
+nrow(sample_data(phyASV.f.c.sediment.2014))
+nrow(sample_data(phyASV.f.c.sediment.2015a))
+nrow(sample_data(phyASV.f.c.sediment.2015b))
+
